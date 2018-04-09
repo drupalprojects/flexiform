c78d1d159fad7c232c39374c5ba7b739e93afbde
rlmumford <rlmumford@googlemail.com>
2018-04-09 11:54:59 +0000

    by rlmumford: First stab at test.

---
+++ contrib/wizard/tests/src/Functional/FlexiformWizardTest.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Drupal\Tests\flexiform_wizard\Functional;
+
+use Drupal\node\Entity\Node;
+use Drupal\Tests\BrowserTestBase;
+
+/**
+ * Basic tests for flexiform wizard.
+ */
+class FlexiformWizardTest extends BrowserTestBase {
+
+  /**
+   * Modules to enable.
+   *
+   * @var array
+   */
+  public static $modules = ['flexiform_wizard_test'];
+
+  /**
+   * Use the standard profile so that we can use the 'page' node type.
+   *
+   * @var string
+   */
+  protected $profile = 'standard';
+
+  /**
+   * Tests that the test flexiform wizard works.
+   */
+  public function testTestFlexiformWizard() {
+    $account = $this->drupalCreateUser(['administer content']);
+    $this->drupalLogin($account);
+
+    // Create a test page.
+    $node = Node::create([
+      'type' => 'page',
+      'title' => 'Test Page Node',
+    ]);
+    $node->save();
+
+    $this->drupalGet('flexiform_wizard/test/'.$node->id().'/'.$account->id());
+    $this->assertSession()->statusCodeEquals(200);
+  }
+}
