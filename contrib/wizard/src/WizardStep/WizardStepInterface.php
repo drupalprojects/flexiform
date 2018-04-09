92f223cbeaf202a5efc0b08c204cc4f45e8ab3db
Jeremy Skinner <jeremy@jeremyskinner.co.uk>
2018-04-09 14:06:23 +0100

    Make the form methods explicit on the WizardStep

--- contrib/wizard/src/WizardStep/WizardStepInterface.php
+++ contrib/wizard/src/WizardStep/WizardStepInterface.php
@@ -2,6 +2,9 @@
 
 namespace Drupal\flexiform_wizard\WizardStep;
 
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\user\PrivateTempStore;
+
 /**
  * Interface for wizard steps plugins.
  */
@@ -18,5 +21,38 @@ interface WizardStepInterface {
    *     - form - The fully qualified class name of the form object for this page.
    */
   public function stepInfo($name = '', array $cached_values = []);
+
+  /**
+   * Form constructor.
+   *
+   * @param array $form
+   *   An associative array containing the structure of the form.
+   * @param \Drupal\Core\Form\FormStateInterface $form_state
+   *   The current state of the form.
+   *
+   * @return array
+   *   The form structure.
+   */
+  public function buildForm(array $form, FormStateInterface $form_state);
+
+  /**
+   * Form validation handler.
+   *
+   * @param array $form
+   *   An associative array containing the structure of the form.
+   * @param \Drupal\Core\Form\FormStateInterface $form_state
+   *   The current state of the form.
+   */
+  public function validateForm(array &$form, FormStateInterface $form_state);
+
+  /**
+   * Form submission handler.
+   *
+   * @param array $form
+   *   An associative array containing the structure of the form.
+   * @param \Drupal\Core\Form\FormStateInterface $form_state
+   *   The current state of the form.
+   */
+  public function submitForm(array &$form, FormStateInterface $form_state);
 
 }
